4e2a948b7cbf5a08f0e3d4d78e1e9a6823f9a255
Tyler Martin <tyler.martin@electric-playground.com>
2024-10-13 11:53:28 -0300

    converts vit config to TS file, updates app route with Input and Output components

--- src/routes/app.jsx
+++ src/routes/app.jsx
@@ -1,12 +1,18 @@
+import { useState } from "react";
 import Inputs from "../components/Inputs";
 import Output from "../components/Output";
 
 //TODO: define first iteration of output. For example, a monthly mortgage payment
 export default function App() {
+  const [schedule, setSchedule] = useState(0)
   return (
     <section>
-      <Inputs />
-      <Output />
+      <Inputs 
+        setSchedule={setSchedule}
+      />
+      <Output
+        schedule={schedule}
+      />
     </section>
   )
 }
