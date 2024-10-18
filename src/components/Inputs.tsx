5a4af25e4ad52af23537b692d3f5d8b01de3293d
Tyler Martin <t.mar92@gmail.com>
2024-10-17 22:39:38 -0300

    uses new Input component for interest rate

--- src/components/Inputs.tsx
+++ src/components/Inputs.tsx
@@ -1,51 +1,19 @@
 import React, { useCallback, useState } from "react"
-import { getEffectiveRate, toPercentage } from "../utils/calculators"
 import { debounce } from "../utils/debounce"
 
 interface InputProps {
-  setRate: (val: number) => number,
   setAmortization: (val: number) => number,
   setPaymentType: (val: string) => void,
 }
 
 export default function Inputs({
-  setRate,
   setAmortization,
   setPaymentType,
 }: InputProps) {
 
-  const [interestRate, setInterestRate] = useState<string>('')
-  const [invalidInterest, setInvalidInterest] = useState<boolean | undefined>(undefined)
-  const [userFeedback, setUserFeedback] = useState<string>('')
-
   const [amortizationPeriod, setAmortizationPeriod] = useState<string>('')
   const [invalidAmortizationPeriod, setInvalidAmortizationPeriod] = useState<boolean | undefined>(undefined)
   const [amortizationPeriodFeedback, setAmortizationPeriodFeedback] = useState<string>('')
-
-  const debouncedRateUpdate = useCallback(debounce((val: string) => {
-    if (!val) {
-      setUserFeedback('')
-      setInvalidInterest(undefined)
-      return
-    }
-
-    if (isNaN(parseFloat(val))) {
-      setInvalidInterest(true)
-      setUserFeedback('Please enter a valid percentage')
-      return
-    }
-
-    /*
-      calculating the Effective rate is not required here.
-      I've printed the value here for testing purposes only.
-      -- I really only need to use setRate() to update 'rate' here
-    */
-    const r = parseFloat(val) / 100
-    setRate(r)
-    const effectiveRate = toPercentage(getEffectiveRate(r))
-    setInvalidInterest(false)
-    setUserFeedback(`Effective Rate: ${effectiveRate}`)
-  }, 400), [])
 
   const debouncedAmortizationUpdate = useCallback(debounce((val: string) => {
     if (!val) {
@@ -66,12 +34,6 @@ export default function Inputs({
     setAmortizationPeriodFeedback(`${a} Years`)
   }, 400), [])
 
-  function handleInterestRateInputChange(e: React.ChangeEvent<HTMLInputElement>): void {
-    const val = e.target.value
-    setInterestRate(val) 
-    debouncedRateUpdate(val)
-  }
-
   function handleAmortizationPeriodInputChange(e: React.ChangeEvent<HTMLInputElement>): void {
     const val = e.target.value
     setAmortizationPeriod(val)
@@ -80,18 +42,6 @@ export default function Inputs({
 
   return (
     <div>
-      <label htmlFor="interestRate">
-        Interest Rate
-        <input
-          value={interestRate}
-          aria-invalid={invalidInterest}
-          type="text"
-          name="interestRate"
-          placeholder="5.5% for example"
-          onChange={handleInterestRateInputChange}
-        />
-        <small>{userFeedback}</small>
-      </label>
       <label htmlFor="amortizationPeriod">
         Amortization Period (years)
         <input
