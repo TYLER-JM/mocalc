d53a0b5b24ce7ca5295ad1dde09a889cb0360bba
Tyler Martin <t.mar92@gmail.com>
2024-10-18 22:41:44 -0300

    updates Output component to better handle incomplete data

--- src/components/Output.tsx
+++ src/components/Output.tsx
@@ -1,5 +1,6 @@
 import accounting from "accounting"
 import { getEffectiveRate, getMonthlyPayment, getPaymentByType, getRateByFrequency, PaymentTypes, toDollars, toPercentage } from "../utils/calculators"
+import { OUTPUT_VALUE_STATUS, OutputValues } from "../types/OutputTypes"
 
 interface OutputProps {
   rate: number,
@@ -15,29 +16,48 @@ export default function Output({
   paymentType,
 }: OutputProps) {
 
+  const output: OutputValues = {
+    status: 'incomplete',
+  }
+
   const effectiveRate = getEffectiveRate(rate)
   const monthlyRate = getRateByFrequency(effectiveRate, 12)
+
   const paymentValues = {
     principal,
     rate: monthlyRate,
     amortization: amortization * 12 // to get the months
   }
-  const monthlyPayment = getMonthlyPayment(paymentValues)
-  const monthlyToString = accounting.formatMoney(monthlyPayment, {precision: 2})
-  const customPayment = getPaymentByType(monthlyPayment, paymentType)
-  const customToString = accounting.formatMoney(customPayment, {precision: 2})
+
+  let monthlyPayment = 0
+  let customPayment = 0
+  let customToString = ''
+
+  if (paymentValues.amortization > 0) {
+    let monthlyPayment = getMonthlyPayment(paymentValues)
+    let customPayment = getPaymentByType(monthlyPayment, paymentType)
+    let customToString = accounting.formatMoney(customPayment, {precision: 2})
+    output.status = 'complete'
+    output.amortizationPeriod = amortization
+    output.payment = customToString
+    output.paymentSchedule = paymentType
+    output.effectiveRate = toPercentage(effectiveRate, 4)
+    output.principal = toDollars(principal, 2)
+  }
+  
   return (
     <div>
-      <ul>
-        <li>Payment Schedule: {paymentType}</li>
-        <li>Effective Rate: {toPercentage(effectiveRate)}</li>
-        <li>Monthly Rate: {toPercentage(monthlyRate, 4)}</li>
-        <li>Mortgage Amount: {toDollars(principal, 0)}</li>
-        <li>Amortization Period, Years: {amortization}</li>
-        <li>Amortization Period, Months: {amortization * 12}</li>
-        <li>Monthly Payment: {monthlyToString}</li>
-        <li>Custom Payment: {customToString}</li>
-      </ul>
+      {output.status === OUTPUT_VALUE_STATUS.incomplete &&
+        <p>pending...</p>
+      }
+      {output.status === OUTPUT_VALUE_STATUS.complete && 
+        <ul>
+          <li>Principal: {output.principal}</li>
+          <li>Amortization (Years): {output.amortizationPeriod}</li>
+          <li>Payment Schedule: {output.paymentSchedule}</li>
+          <li>Payment: {output.payment}</li>
+        </ul>
+      }
     </div>
   )
 }
