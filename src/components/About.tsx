e65a4bea328165437f93f60428e7552b3f451445
Tyler Martin <t.mar92@gmail.com>
2024-11-14 22:38:47 -0400

    adds styles for home page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,15 @@
 export default function About() {
   return (
     <section className="about">
-      Here are more details about the project
+      <div className="about-description">
+        <p><span className="font-heading text-lg text-bold text-gradiant">Mo • Calc</span> <span className="text-sm italic">(n.)</span></p>
+        <p>
+          From <span className="italic text-gradiant">Mo</span>rtgage <span className="text-gradiant italic">Calc</span>ulator,
+          or <span className="text-gradiant italic">M</span>ore than <span className="text-gradiant italic">O</span>ne <span className="text-gradiant italic">Calc</span>ulator,
+          or maybe <span className="text-gradiant italic">Mo</span>re <span className="text-gradiant italic">Calc</span>ulators...
+        </p>
+        <p>Regardless of the name, <span className="text-gradiant">MoCalc</span> can help you compare different Mortgage scenarios quickly.</p>
+      </div>
     </section>
   )
 }
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -3,7 +3,7 @@ import About from "../components/About.tsx";
 export default function Home() {
 	return (
 		<>
-			<div className="home"><h1 className="animate-glide">MoCalc</h1></div>
+			<div className="home"><h1>MoCalc</h1></div>
 			<About />
 		</>
 	)
