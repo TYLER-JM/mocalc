1f34c04cd3d6a75d73a55159535d760f13ecbc2d
Tyler Martin <t.mar92@gmail.com>
2024-11-20 19:22:43 -0400

    styling links in help section

--- src/routes/help.tsx
+++ src/routes/help.tsx
@@ -1,3 +1,5 @@
+import {Link} from "react-router-dom";
+
 export default function Help() {
   function addCalculatorAndRedirect() {
 
@@ -7,12 +9,12 @@ export default function Help() {
     <section className="section">
       <div className="description">
         <p>
-          Use the <button title="click to add a calculator" onClick={addCalculatorAndRedirect}>+</button> button to add up to 8 calculators for easy comparison between various mortgage scenarios.</p>
+          Use the <button className="inline-button" title="click to add a calculator" onClick={addCalculatorAndRedirect}>+</button> button to add up to 8 calculators for easy comparison between various mortgage scenarios.</p>
         <p>
           The payment schedule for the entire term length (up to 5 years) can be viewed in a single table, or viewed as individual tabs for each year of the term.
           A yearly summary shows the <span className="italic">interest</span> paid, the amount of <span className="italic">principal</span> paid down, and the <span className="italic">total</span> amount paid over that year.
         </p>
-        <span onClick={addCalculatorAndRedirect}>Get Started!</span>
+        <button className="link-button"><Link className="output-waiting italic" to="/calculators">Get Started <span>&rarr;</span></Link></button>
       </div>
     </section>
   )
