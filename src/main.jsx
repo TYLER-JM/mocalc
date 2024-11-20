ed49067c20effbc5dd37ddb8266654f3bc13fe76
Tyler Martin <t.mar92@gmail.com>
2024-11-19 22:03:39 -0400

    renames help element to typescript

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './main.css'
 import Root from './Root.tsx'
-import Help from './routes/help.jsx'
+import Help from './routes/help.tsx'
 import Calculators from "./routes/calculators.tsx";
 import Home from "./routes/home.tsx";
 
