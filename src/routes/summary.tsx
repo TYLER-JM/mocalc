4088ffc727372e0d4b94e2241bfce7f361d5e1be
Tyler Martin <t.mar92@gmail.com>
2024-12-30 14:14:53 -0500

    styling progress bars indicating amounts paid

--- src/routes/summary.tsx
+++ src/routes/summary.tsx
@@ -3,12 +3,19 @@ import {useCalculators} from "../Root.tsx";
 
 export default function Summary() {
 	const [calculators] = useCalculators()
+	const dynamicStyles = {
+		width: `${50+25}%`
+	}
 
 	return (
 		<section className="section summary">
 			{calculators && calculators.map(calculator => (
 				<div className="card" key={calculator.id}>
-					<div className="output-summary">
+					<div>
+						<div className="payment-wrapper">
+							<p className="output-summary-header">your total payment will be</p>
+							<p className="output-summary-payment">$18,245</p>
+						</div>
 						<div className="principal-wrapper">
 							<p className="output-summary-header">amount borrowed:</p>
 							<p>$350,000</p>
@@ -21,10 +28,23 @@ export default function Summary() {
 							<p className="output-summary-header">payment schedule:</p>
 							<p>Monthly</p>
 						</div>
-						<div className="payment-wrapper">
-							<p className="output-summary-header">your total payment will be</p>
-							<p className="output-summary-payment">$18,245</p>
+
+						<div className="payment summary">
+							<div className="principal-summary">
+								<div className="summary-bar wrapper">
+									<div className="summary-bar total"></div>
+									<div style={dynamicStyles} className="summary-bar remaining"></div>
+								</div>
+								<div className="principal remaining">$294,000</div>
+								<div className="principal paid">$56,000</div>
+							</div>
+
+							<div className="interest-summary">
+								<div style={dynamicStyles} className="summary-bar interest"></div>
+								<div className="interest paid">$72,000</div>
+							</div>
 						</div>
+
 					</div>
 				</div>
 			))}
