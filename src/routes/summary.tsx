c17380f52ae9fe155d218d93f52f33c0049d1d38
Tyler Martin <t.mar92@gmail.com>
2024-12-30 14:27:56 -0500

    adds labels for interest and principal amounts

--- src/routes/summary.tsx
+++ src/routes/summary.tsx
@@ -13,19 +13,19 @@ export default function Summary() {
 				<div className="card" key={calculator.id}>
 					<div>
 						<div className="payment-wrapper">
-							<p className="output-summary-header">your total payment will be</p>
-							<p className="output-summary-payment">$18,245</p>
+							<p className="summary-header">your total payment will be</p>
+							<p className="output-summary-payment">$1,845</p>
 						</div>
 						<div className="principal-wrapper">
-							<p className="output-summary-header">amount borrowed:</p>
+							<p className="summary-header">amount borrowed:</p>
 							<p>$350,000</p>
 						</div>
 						<div className="rate-wrapper">
-							<p className="output-summary-header">interest rate:</p>
+							<p className="summary-header">interest rate:</p>
 							<p>4.35%</p>
 						</div>
 						<div className="schedule-wrapper">
-							<p className="output-summary-header">payment schedule:</p>
+							<p className="summary-header">payment schedule:</p>
 							<p>Monthly</p>
 						</div>
 
@@ -35,13 +35,19 @@ export default function Summary() {
 									<div className="summary-bar total"></div>
 									<div style={dynamicStyles} className="summary-bar remaining"></div>
 								</div>
-								<div className="principal remaining">$294,000</div>
-								<div className="principal paid">$56,000</div>
+								<div className="label principal remaining">
+									<span className="summary-header">remaining balance:</span>
+									<span>$294,000</span>
+								</div>
+								<div className="label principal paid">
+									<span className="summary-header">principal paid:</span>
+									<span>$56,000</span>
+								</div>
 							</div>
 
 							<div className="interest-summary">
 								<div style={dynamicStyles} className="summary-bar interest"></div>
-								<div className="interest paid">$72,000</div>
+								<div className="label interest paid">$72,000</div>
 							</div>
 						</div>
 
