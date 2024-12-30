647256da8a5705675751531c3d3e60dc319a17e4
Tyler Martin <t.mar92@gmail.com>
2024-12-30 09:46:00 -0500

    import useCalculator hook in summary page to retrieve calculators state

--- src/routes/summary.tsx
+++ src/routes/summary.tsx
@@ -1,21 +1,54 @@
 import "../styles/summary.css";
+import {useCalculators} from "../Root.tsx";
 
 export default function Summary() {
+	const [calculators] = useCalculators()
 
 	return (
 		<section className="section summary">
-			<div className="card">
-				this is a summary
-			</div>
-			<div className="card">
-				this is a summary
-			</div>
-			<div className="card">
-				this is a summary
-			</div>
-			<div className="card">
-				this is a summary
-			</div>
+			{calculators && calculators.map(calculator => (
+				<div className="card" key={calculator.id}>
+					<div className="output-summary">
+						<div className="principal-wrapper">
+							<p className="output-summary-header">amount borrowed:</p>
+							<p>$350,000</p>
+						</div>
+						<div className="rate-wrapper">
+							<p className="output-summary-header">interest rate:</p>
+							<p>4.35%</p>
+						</div>
+						<div className="schedule-wrapper">
+							<p className="output-summary-header">payment schedule:</p>
+							<p>Monthly</p>
+						</div>
+						<div className="payment-wrapper">
+							<p className="output-summary-header">your total payment will be</p>
+							<p className="output-summary-payment">$18,245</p>
+						</div>
+					</div>
+				</div>
+			))}
+
+			{/*<div className="card">*/}
+			{/*	<div className="output-summary">*/}
+			{/*		<div className="principal-wrapper">*/}
+			{/*			<p className="output-summary-header">amount borrowed:</p>*/}
+			{/*			<p>$350,000</p>*/}
+			{/*		</div>*/}
+			{/*		<div className="rate-wrapper">*/}
+			{/*			<p className="output-summary-header">interest rate:</p>*/}
+			{/*			<p>4.35%</p>*/}
+			{/*		</div>*/}
+			{/*		<div className="schedule-wrapper">*/}
+			{/*			<p className="output-summary-header">payment schedule:</p>*/}
+			{/*			<p>Monthly</p>*/}
+			{/*		</div>*/}
+			{/*		<div className="payment-wrapper">*/}
+			{/*			<p className="output-summary-header">your total payment will be</p>*/}
+			{/*			<p className="output-summary-payment">$18,245</p>*/}
+			{/*		</div>*/}
+			{/*	</div>*/}
+			{/*</div>*/}
 		</section>
 	)
 }
